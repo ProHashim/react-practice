34b4e27ee66af75b9269ba81aa6f469640d59c0f
Prohashim <prohashim4e@gmail.com>
2021-10-07 03:06:45 +0500

    Updates

---
+++ precache-manifest.c1432de682c8d7ab563cd6893e93ac07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c74315abf3425b9e4d764eeb620dc303",
+    "url": "/react-practice/index.html"
+  },
+  {
+    "revision": "b81a0ce5bf83c27d624a",
+    "url": "/react-practice/static/css/main.40c592de.chunk.css"
+  },
+  {
+    "revision": "28c81540885fbd63138c",
+    "url": "/react-practice/static/js/2.229c6f10.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-practice/static/js/2.229c6f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b81a0ce5bf83c27d624a",
+    "url": "/react-practice/static/js/main.b0af3a3b.chunk.js"
+  },
+  {
+    "revision": "00bca97b1f6f46f6ebd7",
+    "url": "/react-practice/static/js/runtime-main.f00598a3.js"
+  }
+]);
